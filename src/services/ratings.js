eb51a8b3186cc6941007e81cf9dfc5db0558c90e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-22 23:33:17 -0300

    implement list of ratings

---
+++ src/services/ratings.js
@@ -0,0 +1,18 @@
+export const Ratings = [
+  {
+    id: 'G1',
+    value: 'Leve'
+  },
+  {
+    id: 'PG',
+    value: 'Moderado'
+  },
+  {
+    id: 'PG-13',
+    value: 'Pesado'
+  },
+  {
+    id: 'R',
+    value: 'Muito Pesado'
+  }
+]
