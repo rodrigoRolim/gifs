3596ab2dc57632b832e7431e845e3d097916fb18
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-22 23:30:03 -0300

    import styles for colors

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,29 @@ export default {
 </script>
 
 <style>
-* {
+@import './styles/__colors.scss';
+*,
+*::before,
+*::after {
   box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 body {
-  margin: 0;
-  padding: 0;
   background-color: #2c3e50;
 }
+
 #app {
+  display: flex;
+  flex-direction: column;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  line-height: 1.1;
+  letter-spacing: .3px
 }
 
 </style>
