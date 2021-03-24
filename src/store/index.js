ec2c60fd3cee1954ef520f0d4582e55a97de7ad9
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-24 15:04:02 -0300

    import favorites store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { createStore } from 'vuex'
 import gifs from './gifs';
+import favorites from './favorites';
+
 export default createStore({
   modules: {
+    favorites,
     gifs
   }
 })
