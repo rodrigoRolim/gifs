3612f522c8f9187a0e0c9a4f692b309d6f9858f7
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-24 15:03:00 -0300

    import new icons

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEllipsisH, faSearch, faHeart, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisH, faSearch, faHeart, faTimes, faTrash, faPen } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add({faEllipsisH, faSearch, faHeart, faTimes })
+library.add({faEllipsisH, faSearch, faHeart, faTimes, faTrash, faPen })
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
