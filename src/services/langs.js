fdfe9d0ca73673304b51c76b743a67e84763f7be
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-24 15:03:49 -0300

    create service of languages

---
+++ src/services/langs.js
@@ -0,0 +1,26 @@
+export const Languages = [
+  {
+    id: 'pt',
+    value: 'portugues'
+  },
+  {
+    id: 'en',
+    value: 'inglês'
+  },
+  {
+    id: 'es',
+    value: 'espanhol'
+  },
+  {
+    id: 'de',
+    value: 'alemão'
+  },
+  {
+    id: 'ja',
+    value: 'japonês'
+  },
+  {
+    id: 'it',
+    value: 'italiano'
+  }
+]
