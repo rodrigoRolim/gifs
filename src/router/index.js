58844328f408a5c98a70030e9c1d866b4fcdf557
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-24 15:03:25 -0300

    add new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/Home'
+import Favorites from '../views/Favorites';
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/favoritos',
+    name: 'Favorites',
+    component: Favorites
   }
 ]
 
